37b9cc83d0fddc2624e2dbf59bf9e799f1748e12
Raja Osama <raja.osama@albathanext.com>
2023-01-02 16:08:14 +0400

    bit changes

--- android/src/main/java/com/supersami/foregroundservice/MainActivity.java
+++ android/src/main/java/com/supersami/foregroundservice/MainActivity.java
@@ -11,7 +11,7 @@ import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ReactActivity {
 
       public boolean isOnNewIntent = false;
 
