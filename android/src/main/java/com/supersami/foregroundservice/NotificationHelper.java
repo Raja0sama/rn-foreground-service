64f1b70b289a713fa5d7228eb12ec1761ce2b1e4
davidliu <davidliu@deviange.net>
2024-05-01 20:37:45 +0900

    Fix compile error in NotificationHelper

--- android/src/main/java/com/supersami/foregroundservice/NotificationHelper.java
+++ android/src/main/java/com/supersami/foregroundservice/NotificationHelper.java
@@ -14,6 +14,7 @@ import android.os.Bundle;
 import androidx.core.app.NotificationCompat;
 import android.util.Log;
 
+import com.facebook.react.R;
 
 // partially took ideas from: https://github.com/zo0r/react-native-push-notification/blob/master/android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationHelper.java
 
