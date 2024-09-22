dd6fa795d13bc4877e9bb7e6fe9bb76321eaf056
Raja Osama <rajaosama.me@gmail.com>
2024-09-22 13:04:20 +0400

    Update postinstall.js

--- postinstall.js
+++ postinstall.js
@@ -1,8 +1,5 @@
 const fs = require("fs");
 
-const foregroundServicePermTemplate = `
- <!-- <uses-permission android:name="android.permission.FOREGROUND_SERVICE_LOCATION"/> declare permission like this according to your use case https://developer.android.com/about/versions/14/changes/fgs-types-required -->
-`;
 const metadataTemplate = `
   <meta-data
     android:name="com.supersami.foregroundservice.notification_channel_name"
@@ -16,9 +13,8 @@ const metadataTemplate = `
     android:name="com.supersami.foregroundservice.notification_color"
     android:resource="@color/blue"
   />
- 
-  <service android:name="com.supersami.foregroundservice.ForegroundService"></service> // also define android:foregroundServiceType="" according to your use case
-  <service android:name="com.supersami.foregroundservice.ForegroundServiceTask"></service> // also define android:foregroundServiceType="" according to your use case
+  <service android:name="com.supersami.foregroundservice.ForegroundService"></service>
+  <service android:name="com.supersami.foregroundservice.ForegroundServiceTask"></service>
 `;
 
 const androidManifestPath = `${process.cwd()}/android/app/src/main/AndroidManifest.xml`;
@@ -26,21 +22,6 @@ const androidManifestPath = `${process.cwd()}/android/app/src/main/AndroidManife
 fs.readFile(androidManifestPath, "utf8", function (err, data) {
   if (err) {
     return console.log(err);
-  }
-
-  if (!data.includes(foregroundServicePermTemplate)) {
-    const reg = /<manifest[^>]*>/;
-    const content = reg.exec(data)[0];
-
-    const result = data.replace(
-      reg,
-      `${content}\n${foregroundServicePermTemplate}`
-    );
-    console.log({ result });
-
-    fs.writeFile(androidManifestPath, result, "utf8", function (err) {
-      if (err) return console.log(err);
-    });
   }
 
   if (!data.includes(metadataTemplate)) {
@@ -57,21 +38,41 @@ fs.readFile(androidManifestPath, "utf8", function (err, data) {
 });
 
 const colorTemplate = `
-  <resources>
-    <item  name="blue"  type="color">#00C4D1
-    </item>
-    <integer-array  name="androidcolors">
-    <item>@color/blue</item>
-    </integer-array>
-  </resources>
+  <item  name="blue"  type="color">#00C4D1
+  </item>
+  <integer-array  name="androidcolors">
+  <item>@color/blue</item>
+  </integer-array>
 `;
 
 const colorFilePath = `${process.cwd()}/android/app/src/main/res/values/colors.xml`;
 
-fs.writeFile(colorFilePath, colorTemplate, "utf8", function (err) {
+fs.readFile(colorFilePath, "utf8", function (err, data) {
   if (err) {
-    return console.log(err);
+    return console.error(err);
   }
+
+  const reg = /<resources[^>]*>/;
+  const content = reg.exec(data)?.[0];
+
+  if (!content) {
+    fs.writeFile(
+      colorFilePath,
+      `<resources>${colorTemplate}</resources>`,
+      "utf8",
+      function (err) {
+        return console.error(err);
+      }
+    );
+  }
+
+  const result = data.replace(reg, `${content}${colorTemplate}`);
+
+  fs.writeFile(colorFilePath, result, "utf8", function (err) {
+    if (err) {
+      return console.log(err);
+    }
+  });
 
   console.log(`Successfully created color file at ${colorFilePath}`);
 });
